2b0a623f45115ed65c98539fc1eeedbc24f7413d
Brandon Brasson <rocksong622@gmail.com>
2019-03-04 15:18:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,34 @@
+#class Author
+  #attr_accessor :name,:posts
+#  def initialize(name)
+  #3  @name=name
+  #  @posts=[]
+#@  end
+#end
+
+
 class Author
-  attr_accessor :name,:posts
-  def initialize(name)
-    @name=name
-    @posts=[]
+  attr_accessor :name, :posts
+@@post_count = []
+
+  def initialize(name = nil)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+  @@post_count << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
+  end
+
+  def post_count
+  @@post_count
+  return []
   end
 end
