5cf536b47c9bf5c52d78a2cec332053522bc672a
Brandon Brasson <rocksong622@gmail.com>
2019-03-04 15:44:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,19 @@
 class Song
   attr_accessor :name,:artist
+
   def initialize(name)
-    @name=name
-    @artist=[]
+    @name = name
+    @artist = []
   end
-end
+
+
+def artist_name
+   if @author == nil
+ return nil
+   else
+   @author.name
+   end
+  end
+
+
+  end
